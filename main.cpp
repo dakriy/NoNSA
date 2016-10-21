32c31592b14ee6aad66464368e5d242193d1f280
Trevor Stiles <trevor.stiles@wallawalla.edu>
2016-10-21 00:30:10 -0700

    Forgot to add main.cpp to the last commit...

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include <stdio.h>
+#include "logo.h"
+#include "keybase.h"
 
 int main()
 {
+	Logo logo;
+	printf("Welcome to the NoNSA peer-to-peer chat client!\n");
+	logo.printLogo();
+	printf("Currently there is zero functionality. Sorry about that...\n");
+
+	Keybase keybase;
+	printf("Keybase Status: %d\n",keybase.getKeybaseStatus());
 	return 0;
 }
